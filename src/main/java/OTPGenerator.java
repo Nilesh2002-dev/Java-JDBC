8f185b730cf0f2157f64308d8017b844072b5810
Nilesh <nilesh.yadav22@comp.sce.edu.in>
2026-06-03 15:33:39 +0530

    init commit

--- src/main/java/OTPGenerator.java
+++ src/main/java/OTPGenerator.java
@@ -4,7 +4,7 @@ public class OTPGenerator {
     public static void main(String[] args) {
         SecureRandom random = new SecureRandom();
 
-        int otp = 1000000 + random.nextInt(9000000);
+        int otp = 10000000 + random.nextInt(9000000);
 
         System.out.println("Generated OTP: " + otp);
     }
